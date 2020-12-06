7f10b394f95694a2e1ed390b4bc83dfbf3ac3a24
DESKTOP-JUND1NG\aaa <yosshi3gou@yahoo.co.jp>
2020-12-06 11:24:55 +0900

    3D追加

--- deep-learning-from-scratch/ch04/gradient_3d_rotate.py
+++ deep-learning-from-scratch/ch04/gradient_3d_rotate.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-# cf.http://d.hatena.ne.jp/white_wheels/20100327/p3
 import numpy as np
 import matplotlib.pylab as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -27,32 +26,25 @@ def numerical_gradient(f, X):
             grad[idx] = _numerical_gradient_no_batch(f, x)
         return grad
 
-def function_2(x):
+def function_2(x):   #転置して利用する
     if x.ndim == 1:
         return np.sum(x**2)
     else:
         return np.sum(x**2, axis=1)
 
-def tangent_line(f, x):
-    d = numerical_gradient(f, x)
-    y = f(x) - d*x
-    return lambda t: d*t + y
-
 fig = plt.figure()
 ax = Axes3D(fig)
 
 x0 = np.arange(-2, 2.5, 0.5)
-x1 = np.arange(-2, 2.5, 0.5)
-X, Y = np.meshgrid(x0, x1)
-shp = X.shape
-X,Y = X.flatten(), Y.flatten()
-grad = numerical_gradient(function_2, np.array([X, Y]).T).T
-Z = function_2(np.array([X, Y]).T).T
-X,Y,Z = X.reshape(shp), Y.reshape(shp), Z.reshape(shp)
+X, Y = np.meshgrid(x0, x0)
+XF,YF = X.flatten(), Y.flatten()
+grad = numerical_gradient(function_2, np.array([XF, YF]).T).T
+Z = function_2(np.array([XF, YF]).T).T
+Z = Z.reshape(X.shape)
 
 def plot(i):
     plt.cla()
-    ax.quiver(X,Y,Z,-grad[0].reshape(shp),-grad[1].reshape(shp),0
+    ax.quiver(X,Y,Z,-grad[0].reshape(X.shape),-grad[1].reshape(X.shape),0
               , length=0.2, arrow_length_ratio=0.5, color='green')
     ax.plot_wireframe(X,Y,Z, color='blue')
     plt.grid()
@@ -60,10 +52,11 @@ def plot(i):
     plt.ylabel('x1')
     plt.xlim([-2.5, 2.5])
     plt.ylim([-2.5, 2.5])
-    ax.view_init(elev=20, azim=i*10)
+    ax.view_init(elev=15, azim=i*9)
 
-plot(1)
+plot(2)
 plt.draw()
 plt.show()
-ani = animation.FuncAnimation(fig, plot, interval=300,frames=36)
-ani.save('vector_field.gif', writer='pillow')
+ani = animation.FuncAnimation(fig, plot, interval=200,frames=40)
+ani.save('vector_field_animation.gif', writer='pillow',dpi=150)
+
