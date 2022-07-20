7dcbbc77b57d9ba684419013da8f96fd24acc364
DESKTOP-JUND1NG\aaa <yosshi3gou@yahoo.co.jp>
2022-07-20 23:00:18 +0900

    等高線表示

---
+++ deep-learning-from-scratch/ch02/plot2DContour_and_nand_or_xor.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+from and_gate import AND
+from or_gate import OR
+from nand_gate import NAND
+import numpy as np
+import matplotlib.pyplot as plt
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+def plot2DContour(operator):
+    '''0から１の2次元座標で、1なら赤、0なら青に色分けする'''
+    xs = np.arange(0,11,step=1) / 10
+    ys = np.arange(0,11,step=1) / 10
+    xx,yy = np.meshgrid(xs,ys)
+    color = np.zeros((len(xs),len(ys)), dtype=np.int8)
+
+    for ix,x in enumerate(xs):
+        for iy,y in enumerate(ys):
+            color[ix][iy] =operator(x,y)
+
+    # 等高線を描画する
+    plt.contourf(xx, yy, color, cmap='rainbow')
+    plt.colorbar(label="contour level")
+    plt.show()
+
+if __name__ == '__main__':
+    plot2DContour(AND)
+    plot2DContour(NAND)
+    plot2DContour(OR)
+    plot2DContour(XOR)
+    
